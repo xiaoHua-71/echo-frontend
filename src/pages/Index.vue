5eb67c0df76c7f62843094a2f88cb14f75d74547
hanFei <2035346428@qq.com>
2026-07-19 23:37:12 +0800

    fix:修复推荐用户反参

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -52,7 +52,7 @@ const loadData = async () => {
     })
         .then(function (response) {
           console.log('/user/recommend succeed', response);
-          return response?.data?.records;
+          return response?.data;
         })
         .catch(function (error) {
           console.error('/user/recommend error', error);
